265824bb8e6b512cb9d7df1220eb08c7a715fcfa
Alex Harris <grayat03@gmail.com>
2018-01-30 13:42:54 -0500

    Policy Matters 1-30-18

---
+++ js/main-grid.js
@@ -0,0 +1,85 @@
+
+$(document).ready(function() {
+
+  $("#funding").mouseover(function() {
+    $("#funding-des").show();
+    $("#funding-h1").hide();
+  });
+
+  $("#funding").mouseout(function() {
+    $("#funding-des").hide();
+    $("#funding-h1").show();
+  });
+
+  $("#coverage").mouseover(function() {
+    $("#coverage-des").show();
+    $("#coverage-h1").hide();
+  });
+
+  $("#coverage").mouseout(function() {
+    $("#coverage-des").hide();
+    $("#coverage-h1").show();
+  });
+
+  $("#workforce").mouseover(function() {
+    $("#workforce-des").show();
+    $("#workforce-h1").hide();
+  });
+
+  $("#workforce").mouseout(function() {
+    $("#workforce-des").hide();
+    $("#workforce-h1").show();
+  });
+
+  $("#drugs").mouseover(function() {
+    $("#drugs-des").show();
+    $("#drugs-h1").hide();
+  });
+
+  $("#drugs").mouseout(function() {
+    $("#drugs-des").hide();
+    $("#drugs-h1").show();
+  });
+
+  $("#telehealth").mouseover(function() {
+    $("#telehealth-des").show();
+    $("#telehealth-h1").hide();
+  });
+
+  $("#telehealth").mouseout(function() {
+    $("#telehealth-des").hide();
+    $("#telehealth-h1").show();
+  });
+
+  $("#behavioral").mouseover(function() {
+    $("#beh-des").show();
+    $("#beh-h1").hide();
+  });
+
+  $("#behavioral").mouseout(function() {
+    $("#beh-des").hide();
+    $("#beh-h1").show();
+  });
+
+  $("#other").mouseover(function() {
+    $("#other-des").show();
+    $("#other-h1").hide();
+  });
+
+  $("#other").mouseout(function() {
+    $("#other-des").hide();
+    $("#other-h1").show();
+  });
+
+  $("#advocacy").mouseover(function() {
+    $("#advo-des").show();
+    $("#advo-h1").hide();
+  });
+
+  $("#advocacy").mouseout(function() {
+    $("#advo-des").hide();
+    $("#advo-h1").show();
+  });
+
+
+})
